c686839ab3865b2fb623d239c3602b95a54fd60a
Dalton Turner <dcturner555@gmail.com>
2019-02-25 17:24:05 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,62 @@
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor of calculator
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal - gets total
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add - adds value to total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract - subtracts value from total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply - multiplies total by value
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide - divides total by value
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory - gets history of total
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+	
+}
